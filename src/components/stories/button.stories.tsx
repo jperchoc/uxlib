3a4b3df175ca90f7d9024c1d6ad8ae786c06118b
Arthur Bullet <arthur.bullet@ovhcloud.com>
2025-03-11 10:45:58 +0100

    add stories to menu button
    Signed-off-by: Arthur Bullet <arthur.bullet@ovhcloud.com>

--- src/components/stories/button.stories.tsx
+++ src/components/stories/button.stories.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Meta, StoryFn } from "@storybook/react";
 import { Button, ButtonProps } from "../ui/button"; // Adjust the path as needed
+import { MoreHorizontal } from "lucide-react";
 // import { TrashIcon } from "lucide-react";
 
 export default {
@@ -26,6 +27,18 @@ export default {
 
 const Template: StoryFn<ButtonProps> = (args) => <Button {...args} />;
 
+const TemplateMenu: StoryFn<ButtonProps> = (args) => (
+  <Button
+    {...args}
+    data-testid="token-action-trigger"
+    variant="menu"
+    size="menu"
+  >
+    <span className="sr-only">Open menu</span>
+    <MoreHorizontal className="h-4 w-4" />
+  </Button>
+);
+
 export const Default = Template.bind({});
 Default.args = {
   variant: "primary",
@@ -33,3 +46,10 @@ Default.args = {
   size: "md",
   children: "Button",
 };
+
+export const Menu = TemplateMenu.bind({});
+Menu.args = {
+  variant: "menu",
+  mode: "menu",
+  size: "menu",
+}
